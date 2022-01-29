f96bfa7a32ae6beba1434e052e9073261aaaf2b2
1001-caleb <mccalebyt@gmail.com>
2022-01-29 14:45:34 -0500

    validando peso y altura

--- js/main.js
+++ js/main.js
@@ -8,16 +8,27 @@ var pacientes = document.querySelector("#paciente-pablo");
 
 var tdPeso = pacientes.querySelector(".info-peso");
 var peso = tdPeso.textContent;
-console.log(peso)
+// console.log(peso);
 
 var tdAltura = pacientes.querySelector(".info-altura");
 var altura = tdAltura.textContent;
-console.log(altura)
+// console.log(altura);
 
 function calcularIMC (peso, altura){
     var resultadoImc = peso / (altura*altura);
-    console.log(resultadoImc);
+    // console.log(resultadoImc);
     var mandarInfo = document.querySelector(".info-imc").innerHTML = (resultadoImc);
 }
-
 calcularIMC(peso, altura);
+
+function validarPesoAltura (){
+    if (peso <= 0   || peso >= 400){
+        alert("El peso no puede ser menor a cero, ni mayor a 400")
+    }
+    if (altura <= 0   || altura >= 2.50){
+        alert("La altura no puede ser menor a cero ni mayor a 2.50m")
+    }
+}
+
+validarPesoAltura();
+
