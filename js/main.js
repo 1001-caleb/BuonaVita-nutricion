2cfa79d2a176b1334cf71e6cdf691d5347fb90fd
1001-caleb <mccalebyt@gmail.com>
2022-01-29 16:06:52 -0500

    Calculo automatico de todos los imc

--- js/main.js
+++ js/main.js
@@ -3,32 +3,37 @@
 
 //IMC asocia el peso y la altura de una persona, formula: IMC = peso / altura*altura
 
-var pacientes = document.querySelector("#paciente-pablo");
-// console.log(pacientes);
+var pacientes = document.querySelectorAll(".paciente");
+console.log(pacientes);
 
-var tdPeso = pacientes.querySelector(".info-peso");
-var peso = tdPeso.textContent;
-// console.log(peso);
+for (var i = 0; i < pacientes.length; i++){
+    var paciente = pacientes[i];
 
-var tdAltura = pacientes.querySelector(".info-altura");
-var altura = tdAltura.textContent;
-// console.log(altura);
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    console.log(peso);
 
-function calcularIMC (peso, altura){
-    var resultadoImc = peso / (altura*altura);
-    // console.log(resultadoImc);
-    var mandarInfo = document.querySelector(".info-imc").innerHTML = (resultadoImc);
-}
-calcularIMC(peso, altura);
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    console.log(altura);
 
-function validarPesoAltura (){
-    if (peso <= 0   || peso >= 400){
-        alert("El peso no puede ser menor a cero, ni mayor a 400")
+    var tdIMC = paciente.querySelector(".info-imc")
+    console.log(tdIMC);
+
+    function calcularIMC(peso, altura) {
+        var resultadoImc = peso / (altura * altura);
+         console.log(resultadoImc);
+        tdIMC.textContent = resultadoImc.toFixed(2);
     }
-    if (altura <= 0   || altura >= 2.50){
-        alert("La altura no puede ser menor a cero ni mayor a 2.50m")
+    calcularIMC(peso, altura);
+
+    function validarPesoAltura() {
+        if (peso <= 0 || peso >= 400) {
+            alert("El peso no puede ser menor a cero, ni mayor a 400")
+        }
+        if (altura <= 0 || altura >= 2.50) {
+            alert("La altura no puede ser menor a cero ni mayor a 2.50m")
+        }
     }
+    validarPesoAltura();
 }
-
-validarPesoAltura();
-
