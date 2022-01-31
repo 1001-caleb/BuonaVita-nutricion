e4d39920e13b735c9ecc6b65e9ac11c021ba1313
1001-caleb <mccalebyt@gmail.com>
2022-01-31 07:32:54 -0500

    arreglando el error al calcular imc de nuevo input

--- js/form.js
+++ js/form.js
@@ -1,169 +1,36 @@
-var botonAdicionar = document.getElementById("adicionar-paciente");
-botonAdicionar.addEventListener("click", adicionarPaciente);
+var botonAdicionar = document.querySelector("#adicionar-paciente");
 
-function adicionarPaciente(event){
-   event.preventDefault(); // para que no recargue la pagina
-    //console.log("click")
-    var form = document.getElementById("form-adicionar");
-    //console.log(form);
+botonAdicionar.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adicionar");
 
     var nombre = form.nombre.value;
-    var peso= form.peso.value;
-    var altura= form.altura.value;
-    var gordura = form.gordura.value;
-    
-    var tabla = document.querySelector("#tabla-pacientes")
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value
+
+    var tabla = document.querySelector("#tabla-pacientes");
 
     pacienteTr = document.createElement("tr");
-
     nombreTd = document.createElement("td");
-    pesoTd = document.createElement("td");
     alturaTd = document.createElement("td");
+    pesoTd = document.createElement("td");
     gorduraTd = document.createElement("td");
     imcTd = document.createElement("td");
 
     nombreTd.textContent = nombre;
-    pesoTd.textContent = peso;
     alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura; 
+    pesoTd.textContent = peso;
+    gorduraTd.textContent = gordura;
     imcTd.textContent = calcularIMC(peso, altura);
-    
+
     pacienteTr.appendChild(nombreTd);
     pacienteTr.appendChild(pesoTd);
     pacienteTr.appendChild(alturaTd);
     pacienteTr.appendChild(gorduraTd);
     pacienteTr.appendChild(imcTd);
-    console.log(imcTd);
 
     tabla.appendChild(pacienteTr);
 
-    console.log(pacienteTr);
-    
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
--- js/main.js
+++ js/main.js
@@ -1,255 +1,49 @@
-// var title = document.querySelector(".title");
-// title.textContent = "Buena vida nutrición";
-
-//IMC asocia el peso y la altura de una persona, formula: IMC = peso / altura*altura
-
 var pacientes = document.querySelectorAll(".paciente");
-console.log(pacientes);
 
-for (var i = 0; i < pacientes.length; i++){
+
+for(var i = 0;i< pacientes.length;i++){
     var paciente = pacientes[i];
 
     var tdPeso = paciente.querySelector(".info-peso");
     var peso = tdPeso.textContent;
-    console.log(peso);
 
     var tdAltura = paciente.querySelector(".info-altura");
     var altura = tdAltura.textContent;
-    console.log(altura);
 
-    var tdIMC = paciente.querySelector(".info-imc")
-    console.log(tdIMC);
+    var tdIMC = paciente.querySelector(".info-imc");
 
-    function calcularIMC(peso, altura) {
-        var resultadoImc = peso / (altura * altura);
-         console.log(resultadoImc);
-        tdIMC.textContent = resultadoImc.toFixed(2);
-    }
-    calcularIMC(peso, altura);
 
     pesoEsValido = true;
     alturaEsValida = true;
-    function validarPesoAltura() {
-        if (peso <= 0 || peso >= 400) {
-            // alert("El peso no puede ser menor a cero, ni mayor a 400");
-            tdIMC.textContent = "peso incorrecto";
-            pesoEsValido = false;
-            paciente.classList.add("valor-incorrecto");
-        }
-        if (altura <= 0 || altura >= 2.50) {
-            // alert("La altura no puede ser menor a cero ni mayor a 2.50m");
-            tdIMC.textContent = "Altura incorrecta";
-            alturaEsValida = false;
-            paciente.classList.add("valor-incorrecto");
-        }
+
+    if((peso < 0) || (peso > 1000)){
+        console.log("Peso incorrecto");
+        tdIMC.textContent = "Peso incorrecto";
+        pesoEsValido = false;
+        paciente.classList.add("paciente-incorrecto");
     }
-    validarPesoAltura();
+
+    if((altura < 0) || (altura > 3.00)){
+        console.log("Altura incorrecta");
+        tdIMC.textContent = "Altura incorrecta";
+        alturaEsValida = false;
+        paciente.classList.add("paciente-incorrecto");
+    }
+
+    if(pesoEsValido && alturaEsValida){
+        tdIMC.textContent = calcularIMC(peso,altura);
+    }
+
 }
 
-function calcularImc(peso, altura){
-    var imc = peso / (altura*altura);
+function calcularIMC(peso,altura){
+    var imc = peso / (altura * altura);
     return imc.toFixed(2);
 }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
 
 
