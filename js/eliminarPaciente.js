754d512cb616f25549aab602d3acb8158d4595cd
1001-caleb <mccalebyt@gmail.com>
2022-02-01 08:23:14 -0500

    eliminar paciente

--- js/eliminarPaciente.js
+++ js/eliminarPaciente.js
@@ -1,9 +1,13 @@
-var pacientes = document.querySelectorAll(".paciente");
-console.log(pacientes);
 
-pacientes.forEach(function(paciente){
-    paciente.addEventListener("dblclick", function(){
-        // console.log("doble click");
-        this.remove();
-    })
-})
+var tabla = document.querySelector("#tabla-pacientes");
+tabla.addEventListener("dblclick", function(event){
+    //identificamos donde ocurrió el evento
+    event.target.parentNode.remove();
+});
+
+// pacientes.forEach(function(paciente){
+//     paciente.addEventListener("dblclick", function(){
+//         // console.log("doble click");
+//         this.remove();
+//     });
+// });
